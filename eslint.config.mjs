fc0398b872c12272edc19c191ee859de6a561708
hunter850 <hunter850711@gmail.com>
2025-02-20 22:00:36 +0800

    [Chore] add eslint & prettier to dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:prettier/recommended"),
+    {
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "prettier/prettier": "warn",
+        },
+    },
 ];
 
 export default eslintConfig;
